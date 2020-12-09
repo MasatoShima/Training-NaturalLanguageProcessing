316c846234bd3fffa4d4bcc94b84a8738feb362c
MasatoShima <masato.shima@quick.jp>
2020-12-09 14:03:14 +0900

    Updated

--- masato.shima.2020/app/config/rules.lisp
+++ masato.shima.2020/app/config/rules.lisp
@@ -6,6 +6,7 @@
   (#a0.lemma=受ける&.POS=動詞&.POS2=自立
     *
     (.lemma=影響&.POS=名詞&.POS2=サ変接続&.case=を&.casePOS=助詞&.casePOS2=格助詞&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str="")
+    *
   )
 )
 )

2e2f344d6de74ed7857842a073be39b8519f2347
MasatoShima <masato.shima@quick.jp>
2020-12-09 13:31:11 +0900

    Updated

--- masato.shima.2020/example/extract_annotation.py
+++ masato.shima.2020/example/extract_annotation.py
@@ -19,7 +19,12 @@ def main():
 
 	rules = h.compile(name="template1", rules=["data/rules.lisp"], dicts=[])
 
-	sample_text = ["私は上司に書類を提出した"]
+	sample_text = [
+		"主な事業領域ごとの特性として、"
+		"プラント・自動車・建設機械等の機械関連取引、金属資源・エネルギー・化学品等のトレード並びに"
+		"開発投資については世界経済の動向に大きく影響を受ける一方、"
+		"繊維・食料等の生活消費分野は相対的に国内景気の影響を受けやすいと言えます。"
+	]
 
 	rules_parsed = h.parse_sentences(sample_text)
 

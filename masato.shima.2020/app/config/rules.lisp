7abb036c3de1e757e03006d0ab1ba96dd8370ef3
MasatoShima <masato.shima@quick.jp>
2020-12-16 16:20:37 +0900

    Updated

--- masato.shima.2020/app/config/rules.lisp
+++ masato.shima.2020/app/config/rules.lisp
@@ -16,19 +16,63 @@
 
 (
 ; また、世界経済全般のみならず、海外の特定地域に固有の経済動向に加え、近年の急速な技術革新等による産業構造等の変化、グローバル化に伴う新興成長国との競合激化、更には規制緩和や異業種参入等のビジネス環境の変化が、当社グループの既存のビジネスモデルや競争力、将来の財政状態、業績に重要な影響を及ぼす可能性があります。
-; generated: 2020/12/09 07:03:28
+; generated: 2020/12/16 07:15:11
 ((id . template2))
-  (.lemma=ある&.POS=動詞&.POS2=自立&.case=""&.casePOS=""&.casePOS2=""&.symbol=。&.suf=""&.tense=""&.suf_str=""&.tense_str=""
-    *
-    (.lemma=可能性&.POS=名詞&.POS2=接尾&.case=が&.casePOS=助詞&.casePOS2=格助詞&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
-      (.lemma=及ぼす&.POS=動詞&.POS2=自立&.case=""&.casePOS=""&.casePOS2=""&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
-        *
-        (.lemma=影響&.POS=名詞&.POS2=サ変接続&.case=を&.casePOS=助詞&.casePOS2=格助詞&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
-          *
+(.lemma=ある&.POS=動詞&.POS2=自立&.case=""&.casePOS=""&.casePOS2=""&.symbol=。&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+  (.lemma=また&.POS=接続詞&.POS2="*"&.case=""&.casePOS=""&.casePOS2=""&.symbol=、&.suf=""&.tense=""&.suf_str=""&.tense_str="")
+  (.lemma=加える&.POS=動詞&.POS2=自立&.case=""&.casePOS=""&.casePOS2=""&.symbol=、&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+    (.lemma=なる&.POS=動詞&.POS2=自立&.case=""&.casePOS=""&.casePOS2=""&.symbol=、&.suf=ぬ&.tense=""&.suf_str=ぬ&.tense_str=""
+      (.lemma=世界経済全般&.POS=名詞&.POS2=一般&.case=のみ&.casePOS=助詞&.casePOS2=副助詞&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str="")
+    )
+    (.lemma=特定地域&.POS=名詞&.POS2=一般&.case=に&.casePOS=助詞&.casePOS2=格助詞&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+      (.lemma=海外&.POS=名詞&.POS2=一般&.case=の&.casePOS=助詞&.casePOS2=連体化&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str="")
+    )
+    (.lemma=経済動向&.POS=名詞&.POS2=一般&.case=に&.casePOS=助詞&.casePOS2=格助詞&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+      (.lemma=固有&.POS=名詞&.POS2=一般&.case=の&.casePOS=助詞&.casePOS2=連体化&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str="")
+    )
+  )
+  (.lemma=変化&.POS=名詞&.POS2=サ変接続&.case=""&.casePOS=""&.casePOS2=""&.symbol=、&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+    (.lemma=技術革新等&.POS=名詞&.POS2=接尾&.case=による&.casePOS=助詞&.casePOS2=格助詞&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+      (.lemma=近年&.POS=名詞&.POS2=副詞可能&.case=の&.casePOS=助詞&.casePOS2=連体化&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str="")
+      (.lemma=急速&.POS=名詞&.POS2=形容動詞語幹&.case=""&.casePOS=""&.casePOS2=""&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str="")
+    )
+    (.lemma=産業構造等&.POS=名詞&.POS2=接尾&.case=の&.casePOS=助詞&.casePOS2=連体化&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str="")
+  )
+  (.lemma=更に&.POS=副詞&.POS2=助詞類接続&.case=は&.casePOS=助詞&.casePOS2=係助詞&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+    (.lemma=新興成長国&.POS=名詞&.POS2=接尾&.case=との&.casePOS=助詞&.casePOS2=連体化&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+      (.lemma=伴う&.POS=動詞&.POS2=自立&.case=""&.casePOS=""&.casePOS2=""&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+        (.lemma=グローバル化&.POS=名詞&.POS2=接尾&.case=に&.casePOS=助詞&.casePOS2=格助詞&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str="")
+      )
+    )
+    (.lemma=競合激化&.POS=名詞&.POS2=サ変接続&.case=""&.casePOS=""&.casePOS2=""&.symbol=、&.suf=""&.tense=""&.suf_str=""&.tense_str="")
+  )
+  (.lemma=可能性&.POS=名詞&.POS2=接尾&.case=が&.casePOS=助詞&.casePOS2=格助詞&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+    (#a0.lemma=及ぼす&.POS=動詞&.POS2=自立&.case=""&.casePOS=""&.casePOS2=""&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+      (.lemma=変化&.POS=名詞&.POS2=サ変接続&.case=が&.casePOS=助詞&.casePOS2=格助詞&.symbol=、&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+        (.lemma=ビジネス環境&.POS=名詞&.POS2=一般&.case=の&.casePOS=助詞&.casePOS2=連体化&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+          (.lemma=異業種参入等&.POS=名詞&.POS2=接尾&.case=の&.casePOS=助詞&.casePOS2=連体化&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+            (.lemma=規制緩和&.POS=名詞&.POS2=サ変接続&.case=や&.casePOS=助詞&.casePOS2=並立助詞&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str="")
+          )
         )
+      )
+      (.lemma=業績&.POS=名詞&.POS2=一般&.case=に&.casePOS=助詞&.casePOS2=格助詞&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+        (.lemma=財政状態&.POS=名詞&.POS2=一般&.case=""&.casePOS=""&.casePOS2=""&.symbol=、&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+          (.lemma=競争力&.POS=名詞&.POS2=接尾&.case=""&.casePOS=""&.casePOS2=""&.symbol=、&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+            (.lemma=ビジネスモデル&.POS=名詞&.POS2=一般&.case=や&.casePOS=助詞&.casePOS2=並立助詞&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+              (.lemma=既存&.POS=名詞&.POS2=サ変接続&.case=の&.casePOS=助詞&.casePOS2=連体化&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+                (.lemma=当社グループ&.POS=名詞&.POS2=一般&.case=の&.casePOS=助詞&.casePOS2=連体化&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str="")
+              )
+            )
+          )
+          (.lemma=将来&.POS=名詞&.POS2=副詞可能&.case=の&.casePOS=助詞&.casePOS2=連体化&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str="")
+        )
+      )
+      (.lemma=影響&.POS=名詞&.POS2=サ変接続&.case=を&.casePOS=助詞&.casePOS2=格助詞&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str=""
+        (.lemma=重要&.POS=名詞&.POS2=形容動詞語幹&.case=""&.casePOS=""&.casePOS2=""&.symbol=""&.suf=""&.tense=""&.suf_str=""&.tense_str="")
       )
     )
   )
+)
 )
 
 )
